4f8206ee36d79293638987ed7efff0902af96421
aaaknir <katepetrusova2012@gmail.com>
2020-07-18 10:52:03 +0300

    add homework

---
+++ 2. JS/Homework/Task 9/js/script.js
@@ -0,0 +1,9 @@
+let number = 11;
+for (number = 11; number > 0; number--) {
+    document.write ("Number больше нуля<br>");
+}document.write ("Number меньше нуля");
+let apples = 5;
+for (apples = 5; apples < 10; apples++) {
+	document.write ("Дай мне еще яблок <br>");
+}
+document.write ("Больше яблок мне не надо");
