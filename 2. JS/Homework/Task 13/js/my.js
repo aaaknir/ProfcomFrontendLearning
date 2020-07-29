84976a35fee94b3314600c8b3c80b2b884fb3258
aaaknir <katepetrusova2012@gmail.com>
2020-07-29 18:28:25 +0300

    add homework

---
+++ 2. JS/Homework/Task 13/js/my.js
@@ -0,0 +1,3 @@
+let access = document.getElementById("first").innerHTML + document.getElementById("second").innerHTML + document.getElementById("third").innerHTML;
+document.getElementById("last").innerHTML = access;
+
